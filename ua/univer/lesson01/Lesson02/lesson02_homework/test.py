28f132284c182d201079387078de540645366535
Oksana1-prog <oksana.zhuleneva@gmail.com>
2021-02-21 21:22:43 +0200

    Hometask_part_1

---
+++ ua/univer/lesson01/Lesson02/lesson02_homework/test.py
@@ -0,0 +1,7 @@
+n = int(input('Введите положительное число: '))
+total=0.0
+    while>0:
+        n = int(input('Введите положительное число: '))
+        for c in range(n):
+            total = total + n
+                print(total)
